9eb6707429ca2d9acfe189fdce3ded85adbcce21
yakimaguro <maguroshouta@gmail.com>
2022-02-15 13:51:38 +0900

    プロセスダイアログ追加 / add process dialog

--- src/js/compress.js
+++ src/js/compress.js
@@ -1,13 +1,16 @@
 const { ipcRenderer } = require('electron');
+const { shell } = require('electron')
 const ffmpeg = require('fluent-ffmpeg');
-var command = ffmpeg();
-const status = document.getElementById('compress_status');
+var $ = require('jQuery');
 const compressibility = document.getElementById('compressibility');
-const compress_text = document.getElementById('compressibility_value');
+const compress_value = document.getElementById('compressibility_value');
+var progresstext = document.getElementById('progress-text');
+var dialogclose = document.getElementById('dialog-close');
+var cancel = document.getElementById('cancel');
 var val2;
 
 const setCurrentValue = (val) => {
-  compress_text.innerText = '圧縮率' + val;
+  compress_value.innerText = '圧縮率' + val;
   val2 = val;
 };
 
@@ -25,30 +28,59 @@ function Compress_OnInput(e) {
     const filePath = document.getElementById('compress_file').files[0].path;
     const fileName = document.getElementById('compress_file').files[0].name.split(/\.(?=[^.]+$)/)[0];
     const fileExtension = document.getElementById('compress_file').files[0].name.split(/\.(?=[^.]+$)/)[1];
-
+    dialog_open();
+    $(".dialog-close").addClass("disabled");
+    dialogclose.disabled = true;
     ffmpeg()
       .input(filePath)
       .seekInput(0.0)
       .output(return_path + '/' + fileName + '_converted.' + fileExtension)
       .outputOptions('-crf ' + val2)
       .on('progress', function (progress) {
+        document.getElementById('progress-bar').value = parseInt(progress.percent);
+        progresstext.textContent = parseInt(progress.percent) + '%';
         console.log('Processing: ' + progress.percent + '% done');
-        status.textContent = parseInt(progress.percent) + '% 完了しました';
       })
       .on('end', () => {
         console.log('Processing finished !');
-        status.textContent = '処理が正常に終了しました！';
-        setTimeout(() => {
-          status.textContent = '進行中のプロセスはありません';
-          window.location.reload();
-        }, 5000);
+        $(".dialog-close").removeClass("disabled");
+        progresstext.textContent = '完了';
+        dialogclose.disabled = false;
+        cancel.disabled = true;
+        $(".cancel").addClass("disabled");
       })
       .on('error', function (err) {
+        window.location.reload();
         console.log('エラーが発生しました' + err);
-        status.textContent = 'エラーが発生しました' + err;
+        dialogclose.disabled = false;
       })
       .run();
+    $('#open-folder').on('click', function () {
+      shell.openPath(return_path[0]);
+    });
   });
 }
 const compress_file_input = document.getElementById('compress_file');
 compress_file_input.oninput = Compress_OnInput;
+
+$('#dialog-close').on('click', function () {
+  window.location.reload();
+});
+
+$('#cancel').on('click', function () {
+  window.location.reload();
+});
+
+function dialog_open() {
+  $(this).blur();
+  if ($("#dialog-overlay")[0]) return false;
+  $("body").append('<div class="dialog-overlay"></div>');
+  $(".dialog-overlay").fadeIn("fast");
+  $(".process-dialog").fadeIn("fast");
+}
+
+function dialog_close() {
+  $(".process-dialog, .dialog-overlay").fadeOut("fast", function () {
+    $(".dialog-overlay").remove();
+  });
+}
--- src/js/format.js
+++ src/js/format.js
@@ -1,40 +1,70 @@
 const { ipcRenderer } = require('electron');
+const { shell } = require('electron')
 const ffmpeg = require('fluent-ffmpeg');
-var $ = require('jQuery');
-var command = ffmpeg();
+const $ = require('jQuery');
 var format = document.getElementById('format');
-const status = document.getElementById('format_status');
+var progresstext = document.getElementById('progress-text');
+var dialogclose = document.getElementById('dialog-close');
+var cancel = document.getElementById('cancel');
+var errortext = document.getElementById('error-text');
 
 function Format_OnInput(e) {
   const filePath = document.getElementById('format_file').files[0].path;
   const fileName = document.getElementById('format_file').files[0].name.split(/\.(?=[^.]+$)/)[0];
-
-  console.log(fileName);
-
   ipcRenderer.invoke('select_folder').then(function (return_path) {
+    dialog_open();
+    $(".dialog-close").addClass("disabled");
+    dialogclose.disabled = true;
     ffmpeg()
       .input(filePath)
       .seekInput(0.0)
       .output(return_path + '/' + fileName + '_converted.' + format.value)
       .on('progress', function (progress) {
+        document.getElementById('progress-bar').value = parseInt(progress.percent);
+        progresstext.textContent = parseInt(progress.percent) + '%';
         console.log('Processing: ' + progress.percent + '% done');
-        status.textContent = parseInt(progress.percent) + '% 完了しました';
       })
       .on('end', () => {
         console.log('Processing finished !');
-        status.textContent = '処理が正常に終了しました！';
-        setTimeout(() => {
-          status.textContent = '進行中のプロセスはありません';
-          window.location.reload();
-        }, 5000);
+        $(".dialog-close").removeClass("disabled");
+        progresstext.textContent = '完了';
+        dialogclose.disabled = false;
+        cancel.disabled = true;
+        $(".cancel").addClass("disabled");
       })
       .on('error', function (err) {
         console.log('エラーが発生しました' + err);
-        status.textContent = 'エラーが発生しました' + err;
+        errortext.textContent = 'エラーが発生しました' + err;
+        $(".dialog-close").remove("disabled");
+        dialogclose.disabled = false;
       })
       .run();
-    console.log(return_path);
+    $('#open-folder').on('click', function () {
+      shell.openPath(return_path[0]);
+    });
   });
 }
 const format_file_input = document.getElementById('format_file');
 format_file_input.oninput = Format_OnInput;
+
+$('#dialog-close').on('click', function () {
+  window.location.reload();
+});
+
+$('#cancel').on('click', function () {
+  window.location.reload();
+});
+
+function dialog_open() {
+  $(this).blur();
+  if ($("#dialog-overlay")[0]) return false;
+  $("body").append('<div class="dialog-overlay"></div>');
+  $(".dialog-overlay").fadeIn("fast");
+  $(".process-dialog").fadeIn("fast");
+}
+
+function dialog_close() {
+  $(".process-dialog, .dialog-overlay").fadeOut("fast", function () {
+    $(".dialog-overlay").remove();
+  });
+}
